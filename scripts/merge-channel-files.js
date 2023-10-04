fe999b40c488dbe49561f8b1234c9830bfedb8ae
per1234 <accounts@perglass.com>
2023-10-03 18:44:33 -0700

    Add native Apple Silicon target to build workflow
    On every release tag and manual trigger when the "Include builds on non-free runners" checkbox is checked, make the
    Arduino IDE build for native Apple Silicon host in addition to the builds that are always generated by the "Arduino IDE"
    GitHub Actions workflow.
    
    Previously, the build workflow only produced a build for x86-64 (AKA "Intel") macOS hosts. Although it is possible to
    use those builds on Apple Silicon machines via the Rosetta 2 translation software, the performance is significantly
    inferior to a native build. The reason we produced those Apple Silicon builds manually instead of via the same automated
    continuous deployment system as the other builds was that GitHub did not provide the necessary Apple Silicon runner
    machines and Arduino was not capable of setting up such self-hosted machines in a manner that would make them feasible
    for the project maintainers to use. For this reason, the native Apple Silicon builds had to be produced and published
    manually. GitHub hosted Apple Silicon runner machines are now available so we can add the target to the build workflow.
    
    GitHub gives unlimited use of the basic runner machines for workflow runs in public repositories. However, the macOS ARM
    architecture is only provided in runner machines which are classified as "larger runner". Use of these runners is
    charged on a per-minute basis, without any of the free allowances GitHub provides for the normal runners. In order to
    avoid unnecessary expenditures, native Apple Silicon builds must be generated only when there is compelling reason to do
    so. Such a build is needed for every release, so the workflow is configured to always generate the builds when triggered
    by a tag. In addition to releases, Apple Silicon tester builds for pull requests that might have special implications
    for this target. For this reason, the workflow is configured to allow Apple Silicon builds to be triggered manually by a
    repository maintainer.

---
+++ scripts/merge-channel-files.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+if (process.argv.includes('--help') || process.argv.includes('-h')) {
+  console.log(
+    `Usage:
+merge-channel-files.js [FLAG]...
+
+Flags:
+      --channel <name>     The name of the update channel.
+  -h, --help               Print help for the script
+      --input <path>       The path of the folder that contains the files to merge.
+`
+  );
+  process.exit(0);
+}
+
+const channelFlagIndex = process.argv.indexOf('--channel');
+
+let channel;
+if (channelFlagIndex > -1) {
+  channel = process.argv[channelFlagIndex + 1];
+} else {
+  console.error('Missing required --channel flag');
+  process.exit(1);
+}
+
+const inputFlagIndex = process.argv.indexOf('--input');
+
+let channelFilesFolder;
+if (inputFlagIndex > -1) {
+  channelFilesFolder = process.argv[inputFlagIndex + 1];
+} else {
+  console.error('Missing required --input flag');
+  process.exit(1);
+}
+
+// Staging file filename suffixes are named according to `runner.arch`.
+// https://docs.github.com/en/actions/learn-github-actions/contexts#runner-context
+const x86ChannelFilePath = path.join(
+  channelFilesFolder,
+  channel + '-mac-X64.yml'
+);
+const arm64ChannelFilePath = path.join(
+  channelFilesFolder,
+  channel + '-mac-ARM64.yml'
+);
+
+const x86Data = yaml.load(
+  fs.readFileSync(x86ChannelFilePath, { encoding: 'utf8' })
+);
+const arm64Data = yaml.load(
+  fs.readFileSync(arm64ChannelFilePath, { encoding: 'utf8' })
+);
+
+const mergedData = x86Data;
+mergedData['files'] = mergedData['files'].concat(arm64Data['files']);
+
+fs.writeFileSync(
+  path.join(channelFilesFolder, channel + '-mac.yml'),
+  yaml.dump(mergedData, { lineWidth: -1 })
+);
+
+// Clean up by removing staging files.
+fs.rmSync(x86ChannelFilePath);
+fs.rmSync(arm64ChannelFilePath);
